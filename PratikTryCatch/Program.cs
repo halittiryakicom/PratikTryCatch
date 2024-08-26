224be2a669293b54859823e8d1e414ebd1698cc4
Halit TİRYAKİ <halittiryaki61@outlook.com>
2024-08-26 16:31:02 +0300

    Add project files.

---
+++ PratikTryCatch/Program.cs
@@ -0,0 +1,15 @@
+﻿while (true)
+{
+    try
+    {
+        Console.Write("Lütfen bir sayı giriniz: ");
+        string input = Console.ReadLine();
+        int sayi = Convert.ToInt32(input);
+        Console.WriteLine($"Girilen sayı: {sayi}\nGirilen sayının karesi:{sayi*sayi}");
+        break;
+    }
+    catch (FormatException ex)
+    {
+        Console.WriteLine("Geçersiz giriş lütfen sayı giriniz.");
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Try Catch Pratik
+Girilen deðerler sayý ise karesini alan deðil ise hatasýný söyleyip yeniden sayý aldýran program
